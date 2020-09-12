13de33cf5029e376a3081366e70542fba10d60cf
ShiraLotan <timondura@hotmail.com>
2020-09-12 09:19:30 +0300

    Updates

---
+++ precache-manifest.6ff30326818f4e5b7108cce8505d83c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e783da9a8bf9f05d3dfd7e2ef7d174",
+    "url": "/react-dash-master/index.html"
+  },
+  {
+    "revision": "69c89c04b7ae62c46edd",
+    "url": "/react-dash-master/static/css/main.47a39db1.chunk.css"
+  },
+  {
+    "revision": "0036d49372487a178314",
+    "url": "/react-dash-master/static/js/2.4665085d.chunk.js"
+  },
+  {
+    "revision": "81b6e0f34fc5a80a94fad82949fb96e7",
+    "url": "/react-dash-master/static/js/2.4665085d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c89c04b7ae62c46edd",
+    "url": "/react-dash-master/static/js/main.a5dec4aa.chunk.js"
+  },
+  {
+    "revision": "9a39496727a917dafc74",
+    "url": "/react-dash-master/static/js/runtime-main.d5c67b43.js"
+  },
+  {
+    "revision": "7d8e2485d7e81d0345cb6f390c58d70e",
+    "url": "/react-dash-master/static/media/logo.7d8e2485.jpg"
+  }
+]);
